a153544bbbe178721c6c8e6f2fe79d7f280145d1
TomaszBeben <tbeben@st.swps.edu.pl>
2022-09-26 16:26:02 +0200

    redux config in progress

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import App from './App'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
@@ -13,13 +15,15 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <div className='App'>
-      <Routes>
-        <Route path='/*' element={<App />}/>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-    </div>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path='/*' element={<App />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  </Provider>
 );

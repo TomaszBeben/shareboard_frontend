d825fcc3628bf9c1847d1138236b7da52ceed1c9
TomaszBeben <tbeben@st.swps.edu.pl>
2022-10-05 15:27:21 +0200

    grid-training

--- src/components/middleSection/content/Test1.tsx
+++ src/components/middleSection/content/Test1.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import './content.scss'
+
+const fakeinfo = [
+  {
+    info1: 'ofienfowine',
+    info2: 'deijubf',
+    info3: 'mdpoefop',
+  },
+  {
+    info1: 'ofienfowine',
+    info2: 'deijubf',
+    info3: 'mdpoefop',
+  },
+  {
+    info1: 'ofienfowine',
+    info2: 'deijubf',
+    info3: 'mdpoefop',
+  },
+]
 
 const Test1 = () => {
   return (
-    <div>Test1</div>
+    <div className='content-grid-container'>
+      {fakeinfo.map((info, index) => {
+        return (
+
+          <div key={index} className='content-grid-item'>
+            <p>{info.info1}</p>
+            <p>{info.info2}</p>
+            <p>{info.info3}</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
